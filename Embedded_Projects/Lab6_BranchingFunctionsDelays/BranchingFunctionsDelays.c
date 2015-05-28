b2dd0813b4436e71c9818d4cdb8a301b77ee20fe
Chen Dian <okcd00@users.noreply.github.com>
2015-05-28 10:44:15 +0800

    Update BranchingFunctionsDelays.c

--- Embedded_Projects/Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
+++ Embedded_Projects/Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
@@ -38,9 +38,8 @@ The system has one input switch and one output LED. Figure 6.1 shows the system 
 //#define GPIO_PORTF_LOCK_R     (*((volatile unsigned long *)0x40025520))
 //#define GPIO_PORTF_CR_R       (*((volatile unsigned long *)0x40025524))
 // basic functions defined at end of startup.s
-
-	unsigned long In;  // input from PF4
-  unsigned long i; // output to PF2 (blue LED)	
+unsigned long In;  // input from PF4
+unsigned long i; // output to PF2 (blue LED)	
 	
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
@@ -51,31 +50,29 @@ int main(void)
 	unsigned long volatile delay;
 	TExaS_Init(SW_PIN_PF4, LED_PIN_PF2);  // activate grader and set system clock to 80 MHz
 
-  SYSCTL_RCGC2_R |= 0x00000020;				// 1) activate clock for Port F
-  delay = SYSCTL_RCGC2_R;           	// 		allow time for clock to start
+  SYSCTL_RCGC2_R |= 0x00000020;		// 1) activate clock for Port F
+  delay = SYSCTL_RCGC2_R;           	//    allow time for clock to start
 	
   //GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
-  //GPIO_PORTF_CR_R = 0x1F;           // 		allow changes to PF4-0
+  //GPIO_PORTF_CR_R = 0x1F;           //    allow changes to PF4-0
   //only PF0 needs to be unlocked, other bits can't be locked
 	
   GPIO_PORTF_AMSEL_R	&= ~0x14;       // 3) disable analog on PF2 4
   GPIO_PORTF_PCTL_R		&= ~0x000F0F00; // 4) PCTL GPIO on PF2 4
   GPIO_PORTF_DIR_R		&= ~0x10;       // 5) PF4 in(0)
-	GPIO_PORTF_DIR_R		|= 	0x04;				// 5) PF2 out(1)
+  GPIO_PORTF_DIR_R		|= 	0x04;		// 5) PF2 out(1)
   GPIO_PORTF_AFSEL_R	&= ~0x14;       // 6) disable alt funct on PF7-0
-  GPIO_PORTF_PUR_R		|= 	0x10;       // 		enable pull-up on PF0 and PF4
+  GPIO_PORTF_PUR_R		|= 	0x10;       // 	  enable pull-up on PF0 and PF4
   GPIO_PORTF_DEN_R		|= 	0x14;       // 7) enable digital I/O on PF2, 4
-  GPIO_PORTF_DATA_R		|= 	0x04;       // 		The system starts with the LED ON (make PF2 =1). 
+  GPIO_PORTF_DATA_R		|= 	0x04;       // 	  The system starts with the LED ON (make PF2 =1). 
 	
-  EnableInterrupts();           			// 		enable interrupts for the grader
+  EnableInterrupts();           		//    enable interrupts for the grader
   while(1)
 	{
-		Delay100ms(1);										// 		Delay for about 100 ms 100 ms * 80 MHZ/3
+		Delay100ms(1);						// 		Delay for about 100 ms 100 ms * 80 MHZ/3
 		In = GPIO_PORTF_DATA_R&0x10;   		// 		read PF4 into Sw1
-		if (In == 0) 
-		   GPIO_PORTF_DATA_R ^= 0x04;
-		else 
-		   GPIO_PORTF_DATA_R |= 0x04; 
+		if (In == 0)  GPIO_PORTF_DATA_R ^= 0x04;
+		else          GPIO_PORTF_DATA_R |= 0x04; 
   }
 }
 
@@ -84,8 +81,8 @@ void Delay100ms(unsigned long time)
   unsigned long i;
   while(time > 0)
 	{
-    i = 1333333;  // this number means 100ms;
-    while(i > 0)	i = i - 1;
-    time = time - 1; // decrements every 100 ms
+    	i = 1333333;  // this number means 100ms;
+    	while(i > 0)	i = i - 1;
+    	time = time - 1; // decrements every 100 ms
 	}
 }
